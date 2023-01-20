1dd46a15c0bef5202237c1e718faff4f208e5d27
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-01-19 23:06:41 -0300

    Relacionando categorias e artigos

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require("sequelize");
 const connection = require("../database/database");
+const Category = require("../categories/Category");//Importando o model de categoria
 
 const Article = connection.define('articles',{
     title: {
@@ -13,5 +14,10 @@ const Article = connection.define('articles',{
         allowNull: false
     }
 });
+
+Category.hasMany(Article);//Definindo que uma categoria pode conter vários artigos (1 P N);
+Article.belongsTo(Category);//Definindo que um artigo pertence a uma categoria (1 P 1);
+
+//Article.sync({force: true});//Cria a tabela ao ser executada a aplicação
 
 module.exports = Article;
