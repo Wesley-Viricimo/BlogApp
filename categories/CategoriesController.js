a52c0103c6077f373b4b66729ea1e5b47552f237
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-01-20 22:59:14 -0300

    Finalizando formulário de categorias e iniciando com slugify

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("./Category");
+const slugify = require("slugify");
 
 router.get("/admin/categories/new", (req, res) =>{
     res.render("admin/categories/new");
+});
+
+router.post("/categories/save", (req, res) => {
+    let title = req.body.title; //Recupera titulo informado no formulario pelo usuário;
+
+    if(title != undefined){//Se o título for diferente de indefinido...
+        Category.create({ //Salvar no banco de dados, no model de categorias
+            title: title, //Na coluna titulos salvar o titulo informado pelo usuario;
+            slug: slugify(title) //Na coluna slug, transformar a string titulo em uma slug
+        }).then(() => {
+            res.redirect("/");
+        });
+
+    } else {
+        res.redirect("/admin/categories/new");
+    }
 });
 
 module.exports = router;
