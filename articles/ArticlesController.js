b16a43756bc563507147dcca113cf0794c40a12b
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-02-04 21:30:33 -0300

    Finalizando a criação de novo artigo relacionando com categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const slugify = require("slugify");
+const Article = require("./Article");
 
-router.get("/articles", (req,res) => {
+router.get("/admin/articles", (req,res) => {
     res.send("ROTA DE ARTIGOS");
 });
 
 router.get("/admin/articles/new", (req,res) => {
-    res.render("admin/articles/new");
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {categories: categories});
+    })
 });
+
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category; //Recuperando a categoria no qual será escrito o artigo
+
+    if(title != undefined && body != undefined) {
+        Article.create({
+            title: title,
+            slug: slugify(title),
+            body: body,
+            categoryId: category //Salvando na coluna categoryId o id da categoria no qual será escrito o artigo
+        }).then(() => {
+            res.redirect("/admin/articles");//
+        });
+    } else {
+        res.redirect("admin/articles/new");
+    }
+    
+})
 
 module.exports = router;
