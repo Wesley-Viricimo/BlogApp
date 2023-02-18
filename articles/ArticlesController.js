f68924e55fb764d5da7fe8139b308cedb2426611
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-02-18 19:48:50 -0300

    Finalizando atualização de categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -75,7 +75,23 @@ router.get("/admin/articles/edit/:id", (req, res) => {
     }).catch(err => {
         res.redirect("admin/articles");
     });
+});
 
-})
+router.post("/articles/update", (req, res) => {
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.update({title: title, body: body, categoryId: category, slug: slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
 
 module.exports = router;
