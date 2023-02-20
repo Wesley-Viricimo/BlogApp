8973e749108938a2c3ef2ee1c693d102ec2e9607
Wesley Viricimo <wesley.viricimo.silva@outlook.com>
2023-02-19 22:58:05 -0300

    Finalizando validação de usuários e listagem de usuários cadastrados

--- user/UserController.js
+++ user/UserController.js
@@ -2,9 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require("./User");
 const bcrypt = require('bcryptjs');
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 router.get("/admin/users", (req, res) => {
-    res.send("Listagem de usuários");
+    User.findAll().then(users => {
+        res.render("admin/users/index", {users: users});
+    })
 });
 
 router.get("/admin/users/create", (req, res) => {
@@ -16,12 +20,16 @@ router.post("/users/create", (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
 
-    User.findOne({ 
-        where: { //Buscar no banco se o email informado no formulário já existe 
-            email: email
+    User.findOne({ //Verifica se já existem usuários ou emails salvos iguais ao que o usuário informar
+        where: { 
+            [Op.or]: 
+            [
+                {username: username},
+                {email: email}
+            ]
         }
     }).then(user => {
-        if(user == undefined ) { //Se usuário for igual a indefinido significa que ainda não existe no banco de dados
+        if(user == undefined) { //Se usuário for igual a indefinido significa que ainda não existe no banco de dados
             let salt = bcrypt.genSaltSync(10);
             let hash = bcrypt.hashSync(password, salt); //Utilizando a biblioteca bcrypt para gerar um hash com a senha do usuário;
 
@@ -30,9 +38,9 @@ router.post("/users/create", (req, res) => {
                 email: email,
                 password: hash
             }).then(() => {
-                res.redirect("/");
+                res.redirect("/users");
             }).catch(err => {
-                res.redirect("/");
+                res.redirect("/users");
             });
         } else {
            res.redirect("/admin/users/create");
